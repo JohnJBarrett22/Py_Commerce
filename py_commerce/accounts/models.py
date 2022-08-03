53230b303db481785e12d649abd84b803de80e33
John Barrett <johnjbarrett22@gmail.com>
2022-08-02 21:00:27 -0700

    Additional edits made to user class model

--- py_commerce/accounts/models.py
+++ py_commerce/accounts/models.py
@@ -3,6 +3,40 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 # Create your models here.
 
+class MyAccountManager(BaseUserManager):
+    def create_user(self, first_name, last_name, email, password=None):
+        if not email:
+            raise ValueError('User must have an email address!')
+        
+        if not username:
+            raise ValueError('User must have username')
+
+        user = self.model(
+            email = self.normalize_email(email),
+            username = username,
+            first_name = first_name,
+            last_name = last_name
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, first_name, last_name, email, username, password):
+        user = self.model(
+            email = self.normalize_email(email),
+            username = username,
+            password = password,
+            first_name = first_name,
+            last_name = last_name
+        )
+
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superadmin = True
+        user.is_active = True
+        user.save(using=self._db)
+
 class Account(AbstractBaseUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -21,7 +55,13 @@ class Account(AbstractBaseUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
+    objects = MyAccountManager()
+
     def __str__(self):
         return self.email
 
-    # def has_perm(self, perm, obj=None):
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, add_label):
+        return True
