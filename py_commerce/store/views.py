24ff4223c2d931c01904f9c2e17894cd99fe4b9d
John Barrett <johnjbarrett22@gmail.com>
2022-08-05 22:52:13 -0700

    Added more of the logic for filtering query in the store by category

--- py_commerce/store/views.py
+++ py_commerce/store/views.py
@@ -1,5 +1,7 @@
+from unicodedata import category
 from django.shortcuts import get_object_or_404, render
 from . models import Product
+from category.models import Category
 
 # Create your views here.
 
@@ -8,9 +10,13 @@ def store(request, category_slug=None):
     products = None
     
     if category_slug != None:
-        catgories = get_object_or_404(Category, category_slug=slug)
-    products = Product.objects.all().filter(is_available=True)
-    product_count = products.count()
+        catgories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=categories, is_available=True)
+        product_count = products.count()
+    else:
+
+        products = Product.objects.all().filter(is_available=True)
+        product_count = products.count()
 
     context = {
         'products': products,
